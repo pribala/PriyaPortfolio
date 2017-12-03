0673c9b0a25f5c9eb448da34a15107bcd7dd29c1
Priya Balakrishnan <andu_pri@yahoo.com>
2017-12-03 00:53:45 -0500

    feat: portfolio

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -1,24 +1,50 @@
- $("#skillList").click(function(e){
-    e.preventDefault();
-            $(".cardBox").toggleClass("changePosition");
-            $("#card1").animate({
-                'left' : "0",
+ // $("#skillList").click(function(e){
+ //    e.preventDefault();
+ //            $(".cardBox").toggleClass("changePosition");
+ //            $("#card-1").animate({
+ //                'left' : "0",
              
-            })
-            $("#card2").animate({
-                'left' : "12rem",
+ //            })
+ //            $("#card-2").animate({
+ //                'left' : "12rem",
          
-            })
-            $("#card3").animate({
-                'left' : "36rem",
+ //            })
+ //            $("#card-3").animate({
+ //                'left' : "36rem",
         
-            })
-            $("#card4").animate({
-                'left' : "48rem",
+ //            })
+ //            $("#card-4").animate({
+ //                'left' : "48rem",
         
-            })
-            $("#card5").animate({
-                'left' : "24rem",
+ //            })
+ //            $("#card-5").animate({
+ //                'left' : "24rem",
         
-            })
-        });
+ //            })
+ //        });
+
+ $("li").hover(function(){
+    var cardId = $(this).attr("id");
+    switch(cardId) {
+        case "card-1":
+            $("#card-2").toggleClass("blurCard");
+            $("#card-3").toggleClass("blurCard");
+            $("#card-4").toggleClass("blurCard");
+            break;
+        case "card-2":
+            $("#card-1").toggleClass("blurCard");
+            $("#card-3").toggleClass("blurCard");
+            $("#card-4").toggleClass("blurCard");    
+            break;
+        case "card-3":
+            $("#card-2").toggleClass("blurCard");
+            $("#card-1").toggleClass("blurCard");
+            $("#card-4").toggleClass("blurCard"); 
+            break;
+        case "card-4":
+            $("#card-2").toggleClass("blurCard");
+            $("#card-1").toggleClass("blurCard");
+            $("#card-3").toggleClass("blurCard");   
+            break;     
+    }
+ });

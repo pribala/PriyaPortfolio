49597f8f7dffbcb541029b8c52f018bbae527e75
Priya Balakrishnan <andu_pri@yahoo.com>
2017-12-04 16:18:50 -0500

    feat: Add smooth scrolling

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -1,24 +1,38 @@
- $("#skillList").click(function(e){
-    e.preventDefault();
-            $(".cardBox").toggleClass("changePosition");
-            $("#card1").animate({
-                'left' : "0",
-             
-            })
-            $("#card2").animate({
-                'left' : "12rem",
-         
-            })
-            $("#card3").animate({
-                'left' : "36rem",
-        
-            })
-            $("#card4").animate({
-                'left' : "48rem",
-        
-            })
-            $("#card5").animate({
-                'left' : "24rem",
-        
-            })
+// Code to perform a smooth page scroll to an anchor on the same page. 
+
+ // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
         });
+      }
+    }
+  });
